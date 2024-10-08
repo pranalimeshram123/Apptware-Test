768654e33e6b9742d8ebe9fd0b08f4537ecf5256
pranalimeshram123 <126550302+pranalimeshram123@users.noreply.github.com>
2024-10-08 13:07:46 +0530

    Add files via upload
    testSingletonInstance:
    1.This test retrieves the first instance of the Singleton and checks that it is not null.
    2.It then calls getInstance() again to retrieve the second instance and asserts that both instances are the same using isSameAs(). This confirms that only one instance exists.
    testSingletonReflection:
    1.This test also retrieves the first instance of the Singleton.
    2.It uses reflection to try to create a new instance.
    3.The test asserts that an IllegalStateException is thrown when trying to create another instance, and checks that the message contains the expected text.
    4.Finally, it confirms that the original instance is still the same after the reflection update

--- SingletonTest.java
+++ SingletonTest.java
@@ -7,31 +7,36 @@ import org.junit.jupiter.api.Test;
 public class SingletonTest {
 
 	    @Test
-	    @SneakyThrows
-	    void testSingleton() {
+	    void testSingletonInstance() {
 	        // Get the first instance of the Singleton
-	        SingletonTest instance1 = SingletonTest.getInstance();
+	        Singleton instance1 = Singleton.getInstance();
+	        
+	        // Ensure that the instance is not null
+	        Assertions.assertThat(instance1).isNotNull();
+	        
+	        // Get the second instance of the Singleton
+	        Singleton instance2 = Singleton.getInstance();
+	        
+	        // Assert that both instances are the same
+	        Assertions.assertThat(instance1).isSameAs(instance2);
+	    }
+
+	    @Test
+	    @SneakyThrows
+	    void testSingletonReflection() {
+	        // Get the first instance of the Singleton
+	        Singleton instance1 = Singleton.getInstance();
 
 	        // Use reflection to attempt to create a second instance
-	        Constructor<SingletonTest> constructor = SingletonTest.class.getDeclaredConstructor();
+	        Constructor<Singleton> constructor = Singleton.class.getDeclaredConstructor();
 	        constructor.setAccessible(true);
 
 	        // Expect an exception to be thrown when trying to create another instance
 	        Assertions.assertThatThrownBy(() -> constructor.newInstance())
 	                  .isInstanceOf(IllegalStateException.class)
 	                  .hasMessageContaining("Cannot create another instance of Singleton using reflection");
-
+	        
 	        // Ensure the first instance is still the same
-	        Assertions.assertThat(instance1).isSameAs(SingletonTest.getInstance());
+	        Assertions.assertThat(instance1).isSameAs(Singleton.getInstance());
 	    }
-
-		private static SingletonTest getInstance() {
-			// TODO Auto-generated method stub
-			return null;
-		}
 	}
-
-	
-
-
-
